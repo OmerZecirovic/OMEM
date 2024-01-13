b8a539fa4007f07767feb292ea37ede7a27b388d
OmerZecirovic <omer_lav@hotmail.com>
2024-01-13 14:48:40 +0100

    login napravljeno

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,85 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { GoogleLogin } from "react-google-login";
 import firebase from "firebase/compat/app"; // Import Firebase
 import "firebase/compat/auth"; // Import the auth module
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 const LoginPage = () => {
   const router = useRouter();
 
+  const firebaseConfig = {
+    apiKey: "AIzaSyCoyXbEsKBmSfYJ43omZxJ7m07oZgpiLnY",
+    authDomain: "omemwebsite.firebaseapp.com",
+    projectId: "omemwebsite",
+    storageBucket: "omemwebsite.appspot.com",
+    messagingSenderId: "988708156371",
+    appId: "1:988708156371:web:c0d4874b4b977bb6ee7526",
+    measurementId: "G-SPZBKBHYTS",
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
   if (!firebase.apps.length) {
-    const firebaseConfig = {
-      apiKey: "AIzaSyCoyXbEsKBmSfYJ43omZxJ7m07oZgpiLnY",
-      authDomain: "omemwebsite.firebaseapp.com",
-      projectId: "omemwebsite",
-      storageBucket: "omemwebsite.appspot.com",
-      messagingSenderId: "988708156371",
-      appId: "1:988708156371:web:c0d4874b4b977bb6ee7526",
-      measurementId: "G-SPZBKBHYTS",
-    };
     firebase.initializeApp(firebaseConfig);
   }
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleRegularLogin = async () => {
+    try {
+      // Validate form data (add more validation if necessary)
+      if (!formData.email || !formData.password) {
+        console.error("Email and password are required");
+        return;
+      }
+
+      const email = formData.email;
+
+      console.log("Entered email:", email);
+
+      // Check if the email is associated with an existing account
+      const signInMethods = await firebase
+        .auth()
+        .fetchSignInMethodsForEmail(email);
+
+      console.log("Sign-in methods for email:", signInMethods);
+
+      if (signInMethods.length === 50) {
+        // The email is not associated with any account
+        console.error("User not found. Please register first.");
+        // You can set a state variable to show an error message to the user
+        return;
+      }
+
+      // Sign in with email and password
+      await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, formData.password);
+
+      console.log("Login successful!");
+      // Redirect or perform any other action upon successful login
+      router.push("/home");
+    } catch (error) {
+      console.error("Login failed:", error);
+
+      // Handle other login failures
+      console.error("Invalid email or password");
+      // You can set a state variable to show an error message to the user
+    }
+  };
 
   // Handles successful Google login
   const handleGoogleLoginSuccess = (response: any) => {
@@ -54,24 +113,24 @@ const LoginPage = () => {
             <li className="py-2">
               <input
                 type="text"
-                placeholder="Username"
+                name="email"
+                placeholder="Email"
                 className="text-black rounded-md py-2"
+                onChange={handleChange}
               />
             </li>
             <li>
               <input
                 type="password"
-                name=""
-                id=""
+                name="password" // Change from "" to "password"
                 placeholder="Password"
                 className="text-black rounded-md py-2"
+                onChange={handleChange}
               />
             </li>
           </ul>
           <button
-            onClick={() => {
-              router.push("/home");
-            }}
+            onClick={handleRegularLogin}
             className="border-2 rounded-lg mt-5 bg-red-600 py-1 hover:bg-gray-500 px-2"
           >
             Log In
